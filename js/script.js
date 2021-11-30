cf549c653fc3d568971883525ffc66153d1152f0
richard03 <richard@prokoho.cz>
2021-11-30 18:21:54 +0100

    ajax test

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
 function meteoUpdate() {
-    $.get('wpscripts/weather.php', function(data){
-        var weather = data.split(';'),
-            vCur = numeric(weather[2]), 
-            vMin = Math.round(numeric(weather[4])).toString(), 
-            vMax = Math.round(numeric(weather[5]) * 10) / 10, 
-            vSmer = numeric(weather[3]),
-            tlak = Math.round(numeric(weather[16])).toString();
-            rQNH = Math.round(numeric(weather[17])).toString();
+	$.get('http://www.aeroklub.cz/wpscripts/weather.php', function(data) {
+		console.log(data);
+		var weather = data.split(';'),
+			vCur = numeric(weather[2]), 
+			vMin = Math.round(numeric(weather[4])).toString(), 
+			vMax = Math.round(numeric(weather[5]) * 10) / 10, 
+			vSmer = numeric(weather[3]),
+			tlak = Math.round(numeric(weather[16])).toString();
+			rQNH = Math.round(numeric(weather[17])).toString();
 
-        $('.data-wind-cur').html(vSmer + '/' + vMin);
-        $('.data-wind-peak').html(vMin);
-        $('.data-wind-max').html(vMax);
-        $('.data-wind-smer').html(vSmer);
-        $('.data-QNH').html(tlak);
-        $('.data-rQNH').html(rQNH);
-        $('.data-temp').html(numeric(weather[11]));
-        $('.data-date').html(weather[0]);
-        $('.data-time').html(weather[1]);
-        $('.data-SR').html(weather[18]);
-        $('.data-SS').html(weather[19]);
-        });
+		$('.data-wind-cur').html(vSmer + '/' + vMin);
+		$('.data-wind-peak').html(vMin);
+		$('.data-wind-max').html(vMax);
+		$('.data-wind-smer').html(vSmer);
+		$('.data-QNH').html(tlak);
+		$('.data-rQNH').html(rQNH);
+		$('.data-temp').html(numeric(weather[11]));
+		$('.data-date').html(weather[0]);
+		$('.data-time').html(weather[1]);
+		$('.data-SR').html(weather[18]);
+		$('.data-SS').html(weather[19]);
+		});
 
-    $('#kamera-1').attr('src','wpscripts/camera_1.php?' + Math.random());
-    //$('#kamera-2').attr('src','wpscripts/camera_2.php?' + Math.random());
-    $('#kamera-3').attr('src','wpscripts/camera_3.php?' + Math.random());
-    }
-    
+	//$('#kamera-1').attr('src','http://www.aeroklub.cz/wpscripts/camera_1.php?' + Math.random());
+	//$('#kamera-2').attr('src','wpscripts/camera_2.php?' + Math.random());
+	//$('#kamera-3').attr('src','http://www.aeroklub.cz/wpscripts/camera_3.php?' + Math.random());
+	}
+	
 function numeric(data_string) {
-    return data_string.replace(/[a-z]+/ig, ''); 
-    }
+	return data_string.replace(/[a-z]+/ig, ''); 
+}
+
+console.log(data);
