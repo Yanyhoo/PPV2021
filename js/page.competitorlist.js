80c37cda4a545ca453c44717c9ad1bdd015e99ef
Yanyhoo <jiri.janda@elinet.cz>
2021-12-21 16:28:00 +0100

    JSON

--- js/page.competitorlist.js
+++ js/page.competitorlist.js
@@ -1,96 +1,25 @@
-function getCompetitorList() {
-	const url ="https://script.google.com/macros/s/AKfycbyw40My6pltrf9HvvUwvyE8JsW02DH5KMPsnAVRe1cXzRGrkE80luxex4RO1-LfNjFf/exec";
+var myArray = function () {
+	const apiUrl = "https://script.google.com/macros/s/AKfycbyw40My6pltrf9HvvUwvyE8JsW02DH5KMPsnAVRe1cXzRGrkE80luxex4RO1-LfNjFf/exec";
+	const response = await fetch(apiUrl);
+	const data = await response.json();
+};
 
-	fetch(url)
-		.then(d => d.json())
-		.then(d =>{
-			documment.getElementById("app").textContent = d;
-		}),
-
-}
-
-documment.getElementById("btn").addEventListener("click",getCompetitorList);
+function buildTable(data) {
 
 
+	var table = document.getElementById("myTable")
 
+	for (var i = 0; i < data.length; i++) {
+		var row = `<tr>
+						<td>${data[i].name}</td>
+						<td>${data[i].surname}</td>
+						<td>${data[i].aeroclub}</td>
+						<td>${data[i].raceclass}</td>
+						<td>${data[i].glider}</td>
+						<td>${data[i].imatriculation}</td>
+					</tr>`;
+		table.innerHTML += row
 
-//data.filter((x)=>x.code === "JJ");
-
-
-/* // Function to define innerHTML for HTML table
-function show(data) {
-	let tab =
-		`<tr>
-		<th>ID#</th>
-		<th>Name</th>
-		<th>Surname</th>
-		<th>aeroclub</th>
-		<th>raceclass</th>
-		<th>glider</th>
-		<th>imatriculation</th>
-		<th>code</th>
-		<th>email</th>
-		<th>phone</th>
-		<th>paymentDate</th>		
-		</tr>`;
-	// Loop to access all rows 
-	for (let r of data.keys) {
-		let { key, item } = r;
-		tab += `<tr> 
-			<td>${key.Id} </td>
-			<td>${key.name}</td>
-			<td>${key.surname}</td> 
-			<td>${key.aeroclub}</td> 
-			<td>${key.raceclass}</td> 
-			<td>${key.glider}</td> 
-			<td>${key.imatriculation}</td> 
-			<td>${key.code}</td> 
-			<td>${key.email}</td> 
-			<td>${key.phone}</td> 
-			<td>${key.paymentDate}</td> 
-		</tr>
-		<tr>
-			<td>Id</td>
-			<td>name</td>
-			<td>surname</td>
-			<td>aeroclub</td>
-			<td>raceclass</td>
-			<td>glider</td>
-			<td>imatriculation</td>
-			<td>code</td>
-			<td>email</td>
-			<td>phone</td>
-			<td>paymentDate</td>
-		</tr>
-		`;
-		tab += item.map(({ Id, name, surname, aeroclub, raceclass, glider, imatriculation, code, email, phone, paymentDate }) => `<tr>
-				<td>${Id}</td>
-				<td>${name}</td>
-				<td>${surname}</td>
-				<td>${aeroclub}</td>
-				<td>${raceclass}</td>
-				<td>${glider}</td>
-				<td>${imatriculation}</td>
-				<td>${code}</td>
-				<td>${email}</td>
-				<td>${phone}</td>
-				<td>${paymentDate}</td>
-			</tr>`).join();
-		tab += `<tr>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-				</tr>`;
 	}
 
-	// Setting innerHTML as tab variable
-	document.getElementBykey("racing").innerHTML = tab;
-} */
+}
