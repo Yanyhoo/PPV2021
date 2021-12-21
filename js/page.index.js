a93c93a55383286136b6b44b141dcabb39c8a0ef
richard03 <richard@prokoho.cz>
2021-12-21 20:48:25 +0100

    weather.php changed to local

--- js/page.index.js
+++ js/page.index.js
@@ -5,7 +5,7 @@
 	meteoUpdate();
 
 	function meteoUpdate() {
-		$.get('http://ppv2021.prokoho.cz/weather.php', function(data) {
+		$.get('./weather.php', function(data) {
 			var weather = data.split(';'),
 				vCur = numeric(weather[2]), 
 				vMin = Math.round(numeric(weather[4])).toString(), 
@@ -22,12 +22,7 @@
 			$('.data-temp').html(numeric(weather[11]));
 			$('.data-SRS').html(weather[18] + ' - ' + weather[19]);
 
-			// $('.data-wind-peak').html(vMin);
-			// $('.data-wind-smer').html(vSmer);
-			// $('.data-time').html(weather[1]);
-			// $('.data-SR').html(weather[18]);
-			// $('.data-SS').html(weather[19]);
-			});
+		});
 
 		//$('#kamera-1').attr('src','http://www.aeroklub.cz/wpscripts/camera_1.php?' + Math.random());
 		//$('#kamera-2').attr('src','wpscripts/camera_2.php?' + Math.random());
--- weather.php
+++ weather.php
@@ -1,6 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: GET");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: GET");
 
 $ext_weather = 'http://www.aeroklubhb.cz/wpscripts/weather.php';
 $ext_file = @file_get_contents($ext_weather);
