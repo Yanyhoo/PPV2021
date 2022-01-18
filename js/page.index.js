bd0cfadd275550d11b09dc74deb5e282dcdb8b20
richard03 <richard@prokoho.cz>
2022-01-18 20:05:01 +0100

    aktuality update

--- js/page.index.js
+++ js/page.index.js
@@ -46,7 +46,7 @@
 		console.log(0);
 		const response = await fetch(apiUrl);
 		const responseData = await response.json();
-		const displayActuality = responseData[0].data.filter((item) => item.Show === 'TRUE');
+		const displayActuality = responseData[0].data;
 
 		buildActualityList({ data: displayActuality, actualitiesId: 'actualities' })
 
@@ -60,20 +60,22 @@
 		var tableBuffer = [];
 		tableBuffer.push('<ul class="ppv-list">');
 
-		for (var i = 0, n = data.length; i < n; i++) {
-			tableBuffer.push('<li class="ppv-list-item"><h5 class="ppv-list-item__title"><span class="ppv-list-item__time">');
-			tableBuffer.push(data[i].Date);
-			tableBuffer.push(' ');
-			tableBuffer.push(data[i].Time);
-			tableBuffer.push('</span> &nbsp; ');
-			tableBuffer.push(data[i].Header);
-			tableBuffer.push('</h5><p>');
-			tableBuffer.push(data[i].BodyText);
-			tableBuffer.push('</p><p><a href="');
-			tableBuffer.push(data[i].LinkURL);
-			tableBuffer.push('">');
-			tableBuffer.push(data[i].LinkText);		
-			tableBuffer.push('</a></p></li>');
+		for (var i = 0; i < 3; i++) {
+			if (data[i]) {
+				tableBuffer.push('<li class="ppv-list-item"><h5 class="ppv-list-item__title"><span class="ppv-list-item__time">');
+				tableBuffer.push(data[i].Date);
+				tableBuffer.push(' ');
+				tableBuffer.push(data[i].Time);
+				tableBuffer.push('</span> &nbsp; ');
+				tableBuffer.push(data[i].Header);
+				tableBuffer.push('</h5><p>');
+				tableBuffer.push(data[i].BodyText);
+				tableBuffer.push('</p><p><a href="');
+				tableBuffer.push(data[i].LinkURL);
+				tableBuffer.push('">');
+				tableBuffer.push(data[i].LinkText);		
+				tableBuffer.push('</a></p></li>');
+			}
 		}
 		tableBuffer.push('</ul>');
 
